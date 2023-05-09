b0826cdf1979b44f831df38cd05bf405b5f2b620
Welly Winata <winatawelly16@gmail.com>
2023-05-10 00:55:11 +0800

    add navbar

---
+++ src/components/nav/index.tsx
@@ -0,0 +1,27 @@
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+
+import logo from "./logo.svg";
+
+import "./style.css";
+
+const Nav = () => {
+  return (
+    <Navbar bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home" className="navbar-logo">
+          <img
+            src={logo}
+            width="48"
+            height="48"
+            className="d-inline-block align-top"
+            alt="React Bootstrap logo"
+          />
+          PVG-Unspash
+        </Navbar.Brand>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Nav;
