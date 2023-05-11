6f659c4f2c1006f6e98a90557698e7fa939d6314
Welly Winata <winatawelly16@gmail.com>
2023-05-12 00:26:44 +0800

    refactor react-router

--- src/index.tsx
+++ src/index.tsx
@@ -3,26 +3,29 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 import Nav from "./components/nav";
 import Home from "./pages/home";
 import Search from "./pages/search";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  { path: "/search", element: <Search /> },
-]);
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Nav />
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<Search />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
