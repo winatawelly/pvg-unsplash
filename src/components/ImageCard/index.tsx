e6b16c05a92d6a790048566de4056574d34dcabd
Welly Winata <winatawelly16@gmail.com>
2023-05-12 10:15:28 +0800

    add like button

--- src/components/ImageCard/index.tsx
+++ src/components/ImageCard/index.tsx
@@ -1,12 +1,29 @@
 import React from "react";
 import Image from "react-bootstrap/Image";
 
+import loveIcon from "./love-icon.svg";
+import loveIconFilled from "./love-icon-filled.svg";
+
+import "./style.css";
+
 interface Props {
   src: string;
 }
 
 const ImageCard = ({ src }: Props) => {
-  return <Image src={src} width={"100%"} />;
+  const [isLiked, setIsLiked] = React.useState(false);
+  return (
+    <div className="img-card ">
+      <Image src={src} width={"100%"} />
+      <div className="image-card-actions mb-4">
+        <img
+          src={isLiked ? loveIconFilled : loveIcon}
+          className="image-card-action-icon"
+          onClick={() => setIsLiked(!isLiked)}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default ImageCard;
