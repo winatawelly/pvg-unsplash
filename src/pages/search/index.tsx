6aa3bd5434d76f2bbbfac298a32d963aceb6c234
Welly Winata <winatawelly16@gmail.com>
2023-05-14 01:00:38 +0800

    add empty state on search page

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -5,8 +5,11 @@ import { Photo } from "../../schemas/photo";
 
 import ImageCard from "../../components/ImageCard";
 import ImageModal from "../../components/ImageModal";
+import SearchBar from "../../components/searchBar";
 
 import Spinner from "react-bootstrap/Spinner";
+
+import emptyImg from "./empty.svg";
 
 import "./style.css";
 import React from "react";
@@ -14,13 +17,17 @@ import React from "react";
 const Search = () => {
   const navigate = useNavigate();
   const [searchParams] = useSearchParams();
-  const query = searchParams.get("q");
+
+  const [page, setPage] = React.useState(0);
   const [column1, setColumn1] = React.useState<Photo[] | null>(null);
   const [column2, setColumn2] = React.useState<Photo[] | null>(null);
   const [column3, setColumn3] = React.useState<Photo[] | null>(null);
+
   const [selectedImage, setSelectedImage] = React.useState<Photo>();
   const [isModalOpen, setModalOpen] = React.useState(false);
+  const [isEmpty, setIsEmpty] = React.useState(false);
 
+  const query = searchParams.get("q");
   const { data, isLoading, isError } = usePhotos({
     query: query || "",
   });
@@ -29,6 +36,75 @@ const Search = () => {
     setSelectedImage(image);
     setModalOpen(true);
   };
+
+  const loadingRenderer = () => (
+    <div className="loading">
+      <Spinner animation="border" />
+    </div>
+  );
+
+  const imagesRenderer = () => (
+    <>
+      <ImageModal
+        show={isModalOpen}
+        onHide={() => setModalOpen(false)}
+        data={selectedImage}
+      />
+      <div className="column">
+        {column1 &&
+          column1.map((img) => (
+            <ImageCard
+              key={img.id}
+              src={img.urls.small}
+              title={img.description ? img.description : img.alt_description}
+              author={img.user.name}
+              onClick={() => handleImageClick(img)}
+            />
+          ))}
+      </div>
+      <div className="column">
+        {column2 &&
+          column2.map((img) => (
+            <ImageCard
+              key={img.id}
+              src={img.urls.small}
+              title={img.description ? img.description : img.alt_description}
+              author={img.user.name}
+              onClick={() => handleImageClick(img)}
+            />
+          ))}
+      </div>
+      <div className="column">
+        {column3 &&
+          column3.map((img) => (
+            <ImageCard
+              key={img.id}
+              src={img.urls.small}
+              title={img.description ? img.description : img.alt_description}
+              author={img.user.name}
+              onClick={() => handleImageClick(img)}
+            />
+          ))}
+      </div>
+    </>
+  );
+
+  const emptyRenderer = () => (
+    <div id="search-empty">
+      <img src={emptyImg} />
+      <h2 className="mb-0 mt-4">No Result Found</h2>
+      <p>Please try again with another keyword or use more generic term</p>
+      <SearchBar value={query || ""} />
+    </div>
+  );
+
+  React.useEffect(() => {
+    if (!isLoading && data?.length === 0 && page === 0) {
+      setIsEmpty(true);
+    } else {
+      setIsEmpty(false);
+    }
+  }, [data, isLoading, page]);
 
   React.useEffect(() => {
     if (searchParams.get("q") === "" || searchParams.get("q") === null) {
@@ -64,61 +140,11 @@ const Search = () => {
 
   return (
     <div id="search-page">
-      {isLoading ? (
-        <div className="loading">
-          <Spinner animation="border" />
-        </div>
-      ) : (
-        <>
-          <ImageModal
-            show={isModalOpen}
-            onHide={() => setModalOpen(false)}
-            data={selectedImage}
-          />
-          <div className="column">
-            {column1 &&
-              column1.map((img) => (
-                <ImageCard
-                  key={img.id}
-                  src={img.urls.small}
-                  title={
-                    img.description ? img.description : img.alt_description
-                  }
-                  author={img.user.name}
-                  onClick={() => handleImageClick(img)}
-                />
-              ))}
-          </div>
-          <div className="column">
-            {column2 &&
-              column2.map((img) => (
-                <ImageCard
-                  key={img.id}
-                  src={img.urls.small}
-                  title={
-                    img.description ? img.description : img.alt_description
-                  }
-                  author={img.user.name}
-                  onClick={() => handleImageClick(img)}
-                />
-              ))}
-          </div>
-          <div className="column">
-            {column3 &&
-              column3.map((img) => (
-                <ImageCard
-                  key={img.id}
-                  src={img.urls.small}
-                  title={
-                    img.description ? img.description : img.alt_description
-                  }
-                  author={img.user.name}
-                  onClick={() => handleImageClick(img)}
-                />
-              ))}
-          </div>
-        </>
-      )}
+      {isEmpty
+        ? emptyRenderer()
+        : isLoading
+        ? loadingRenderer()
+        : imagesRenderer()}
     </div>
   );
 };
